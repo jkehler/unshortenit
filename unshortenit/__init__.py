b4e7edb1cf84e8ccb4994ddd1bf1784b3eb043c0
Fake-Name <something@fake-url.com>
2016-05-20 20:34:25 -0700

    Kick over revision, update changelog and documentation.

--- unshortenit/__init__.py
+++ unshortenit/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Jeff Kehler'
 __email__ = 'jeffrey.kehler@gmail.com'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from .base import unwrap_30x_only
 from .base import unshorten_only
